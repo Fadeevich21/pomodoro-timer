b86945d01b7b9690c70289a1e4fefac95ae9cc67
Fadeevich21 <fad.danila053@gmail.com>
2023-01-10 15:44:36 +0300

    realizate simple version pomodoro timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,82 @@
 #include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 #include "time.hpp"
 
 #define DELAY_PRINT_TIME 1000
-#define SERIAL_SPEED 9600
+#define TIME_ONE_CYCLES_WORK_POMODORO "00:00:10"
+#define TIME_ONE_CYCLES_SHORT_PAUSE_POMODORO "00:00:05"
+#define TIME_ONE_CYCLES_LONG_PAUSE_POMODORO "00:00:07"
 
+#define SSD1306_ADDRESS 0x3C
+#define SSD1306_WIDTH 128
+#define SSD1306_HEIGHT 64
+
+using SSD1306 = Adafruit_SSD1306;
+
+SSD1306 display(SSD1306_WIDTH, SSD1306_HEIGHT);
 Time time;
 
-void time_setup() {
-    time.set_time(0, 1, 0);
+void time_setup_one_cycle_work_pomodoro() {
+    time.set_time(TIME_ONE_CYCLES_WORK_POMODORO);
+}
+
+void time_setup_one_cycle_short_pause_pomodoro() {
+    time.set_time(TIME_ONE_CYCLES_SHORT_PAUSE_POMODORO);
+}
+
+void time_setup_one_cycle_long_pause_pomodoro() {
+    time.set_time(TIME_ONE_CYCLES_LONG_PAUSE_POMODORO);
+}
+
+void display_setup() {
+    display.begin(SSD1306_SWITCHCAPVCC, SSD1306_ADDRESS);
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setTextSize(2);
 }
 
 void setup() {
-    Serial.begin(SERIAL_SPEED);
-    time_setup();
+    pinMode(13, OUTPUT);
+    display_setup();
 }
 
 void print_time() {
     char* time_str = time.get_time_str();
-    Serial.println(time_str);
+    display.clearDisplay();
+    display.setCursor(15, 0);
+    display.print(time_str);
+    display.display();
 }
 
 void loop() {
-    static bool timer_is_running = true;
+    static uint8_t count_working_cycles_pomodoro = 0;
+    static bool is_working = false;
     static uint64_t milliseconds = millis();
-    if (timer_is_running && millis() - milliseconds > DELAY_PRINT_TIME) {
+    if (millis() - milliseconds > DELAY_PRINT_TIME) {
         milliseconds = millis();
+        print_time();
         if (time != "00:00:00") {
             --time;
-            print_time();
+            if (!is_working) {
+                display.setCursor(4, 32);
+                display.print("Time is up");
+                display.display();
+            }
         } else {
-            Serial.println("Time is up!");
-            timer_is_running = false;
+            if (is_working) {
+                if (count_working_cycles_pomodoro < 3) {
+                    time_setup_one_cycle_short_pause_pomodoro();
+                } else {
+                    time_setup_one_cycle_long_pause_pomodoro();
+                }
+                count_working_cycles_pomodoro = (count_working_cycles_pomodoro + 1) % 4;
+            } else {
+                time_setup_one_cycle_work_pomodoro();
+            }
+
+            is_working = !is_working;
+            digitalWrite(13, is_working);
         }
     }
 }
